8c598f9a77f5e5533835fa2aed48881782cb6ff6
MChnai <baoanhtranngoc024@gmail.com>
2025-09-19 21:02:58 +0700

    Update Main.java

--- Monitor/ProducerConsumerProblem2/src/ProducerConsumer/Main.java
+++ Monitor/ProducerConsumerProblem2/src/ProducerConsumer/Main.java
@@ -26,10 +26,20 @@ package ProducerConsumer;
 public class Main {
 	public static void main(String[] args)
 	{
+		long start = System.nanoTime();
 		Company company = new Company();
 		Thread producer = new Thread(new Producer(company));
 		Thread consumer = new Thread(new Consumer(company));
 		producer.start();
 		consumer.start();
+		
+		try {
+			producer.join();
+			consumer.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		long end = System.nanoTime()-start;
+		System.out.println( "Took  "+ end + " nano seconds");
 	}
 }
